e338d9f3c70a87467b9f941a6efd43be2b17a130
Phibik <75264659+Phibik@users.noreply.github.com>
2024-11-04 10:34:47 +0100

    tasca 3 INCACABADA, tasca 4 FETA

--- CamiBase.h
+++ CamiBase.h
@@ -11,5 +11,14 @@ class CamiBase {
 class CamiSolucio : public CamiBase
 {
 public:
-	std::vector<Coordinate> getCamiCoords() override;
+	std::vector<Coordinate> getCamiCoords() override
+	{	
+		return 
+		{
+			{41.4928803, 2.1452381},
+			{41.4929072, 2.1452474},
+			{41.4933070, 2.1453852},
+			{41.4939882, 2.1456419}
+		}
+	}
 };
--- PuntDeInteresBase.h
+++ PuntDeInteresBase.h
@@ -21,17 +21,59 @@ class PuntDeInteresBase {
 
 class PuntDeInteresBotigaSolucio : public PuntDeInteresBase
 {
+private:
+	std::string shop; 
+	std::string hores; //opening_hours
+	bool discapacitat; //wheelchair
 public:
-	std::string getName() override
-	{
+	PuntDeInteresBotigaSolucio() {} // TODO --> creo que hay que inicializar 
+									//"shop", "opening_hours"... con el xml
 
+	std::string getName() override { return PuntDeInteresBase::getName(); }
+	unsigned int getColor() override
+	{
+		unsigned int color = PuntDeInteresBase::getColor();
+		if (shop == "supermarket")
+			color = 0xA5BE00;
+		else if (shop == "tobacco")
+			color = 0xFFAD69;
+		else if (shop == "bakery")
+		{
+			// std::string::find(x) retorna std::string::npos si no troba x
+			if (hores.find("06:00-22:00") != std::string::npos && discapacitat)
+				color = 0x4CB944;
+			else
+				color = 0xE85D75;
+		}
+		else
+			color = 0xEFD6AC;
+
+		return color;
 	}
-	unsigned int getColor() override;
 };
 
 class PuntDeInteresRestaurantSolucio : public PuntDeInteresBase
 {
+private:
+	std::string cuina; //cuisine
+	bool discapacitat; //wheelchair
 public:
-	std::string getName() override;
-	unsigned int getColor() override;
+	// TODO --> constructor
+
+	std::string getName() override { return PuntDeInteresBase::getName(); }
+	unsigned int getColor() override
+	{
+		// valor per defecte, pot cambiar si es "pizza", "chinese"...
+		unsigned int color = PuntDeInteresBase::getColor();
+
+		if (cuina == "pizza")
+			color = 0x04FCBA;
+		else if (cuina == "chinese")
+			color = 0xA6D9F7;
+
+		if (discapacitat)
+			color = 0x251351;
+
+		return color;
+	}
 };
