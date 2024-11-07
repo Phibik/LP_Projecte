40dfa91d023b30814c95a5951599ade582d5f783
NIU1751116 <amin.elalam@autonoma.cat>
2024-11-07 12:16:07 +0100

    Update MapaBase.h per crear estrucutruas de dades dinamiques

--- MapaBase.h
+++ MapaBase.h
@@ -8,25 +8,22 @@
 
 class MapaBase {
 
-	public:
-		virtual void getPdis(std::vector<PuntDeInteresBase*>&) = 0; // potser s'ha de posar pdis
-		virtual void getCamins(std::vector<CamiBase*>&) = 0;
-		virtual void parsejaXmlElements(std::vector<XmlElement>& xmlElements) = 0;
+public:
+	virtual void getPdis(std::vector<PuntDeInteresBase*>&) = 0; // potser s'ha de posar pdis
+	virtual void getCamins(std::vector<CamiBase*>&) = 0;
+	virtual void parsejaXmlElements(std::vector<XmlElement>& xmlElements) = 0;
 
 };
 
-class MapaSolucio: public MapaBase
-{
+
+class MapaSolucio : public MapaBase {
+private:
+	std::vector<PuntDeInteresBase*> puntsDeInteres;  
+	std::vector<CamiBase*> camins;                  
+
 public:
-	void getPdis(std::vector<PuntDeInteresBase*>& pdis) override
-	{
-		Coordinate cBotiga = { 41.4918606, 2.1465411 };
-		Coordinate cRestaurant = { 41.4902204, 2.1406477 };
-		PuntDeInteresBase* botiga = new PuntDeInteresBotigaSolucio(cBotiga, "La Millor Pastisseria",
-			"bakery");
-		PuntDeInteresBase* restaurant = new PuntDeInteresRestaurantSolucio(cRestaurant, 
-			"El Millor Restaurant", "regional", "yes");
-		pdis = { botiga, restaurant };
+	void getPdis(std::vector<PuntDeInteresBase*>& pdis) override {
+		pdis = puntsDeInteres;
 	}
 
 	void getCamins(std::vector<CamiBase*>& cami) override
@@ -37,4 +34,5 @@ public:
 
 	void parsejaXmlElements(std::vector<XmlElement>& xmlElements) override;
 };
+
 
