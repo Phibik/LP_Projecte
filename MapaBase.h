396dc3a692c6b4e3f3142c56197f4de19607989d
Phibik <75264659+Phibik@users.noreply.github.com>
2024-11-08 10:34:07 +0100

    TASCA 7 (només els nodes, falten els ways)

--- MapaBase.h
+++ MapaBase.h
@@ -4,35 +4,111 @@
 #include "Common.h"
 #include "PuntDeInteresBase.h"
 #include "CamiBase.h"
+#include "Util.h"
 
 
 class MapaBase {
 
-public:
-	virtual void getPdis(std::vector<PuntDeInteresBase*>&) = 0; // potser s'ha de posar pdis
-	virtual void getCamins(std::vector<CamiBase*>&) = 0;
-	virtual void parsejaXmlElements(std::vector<XmlElement>& xmlElements) = 0;
+	public:
+		virtual void getPdis(std::vector<PuntDeInteresBase*>&) = 0;
+		virtual void getCamins(std::vector<CamiBase*>&) = 0;
+		virtual void parsejaXmlElements(std::vector<XmlElement>& xmlElements) = 0;
 
 };
 
-
-class MapaSolucio : public MapaBase {
+class MapaSolucio : public MapaBase
+{
 private:
-	std::vector<PuntDeInteresBase*> puntsDeInteres;  
-	std::vector<CamiBase*> camins;                  
-
+	std::vector<PuntDeInteresBase*> puntsDeInteres;
+	std::vector<CamiBase*> camins;
 public:
-	void getPdis(std::vector<PuntDeInteresBase*>& pdis) override {
+	void getPdis(std::vector<PuntDeInteresBase*>& pdis) override
+	{
 		pdis = puntsDeInteres;
 	}
 
 	void getCamins(std::vector<CamiBase*>& cami) override
 	{
-		CamiBase* camiSol = new CamiSolucio;
-		cami = { camiSol };
+		cami = camins;
 	}
 
-	void parsejaXmlElements(std::vector<XmlElement>& xmlElements) override;
+	void parsejaXmlElements(std::vector<XmlElement>& xmlElements) override
+	{
+		for (const auto& element : xmlElements)
+		{
+			if (element.id_element == "node")
+			{
+				// variables per pdi botiga i restaurant
+				double lat = 0.0, lon = 0.0;
+				std::string name = "";
+				bool botiga = false, restaurant = false;
+				std::string shop = "", rest = "";
+				std::string hores = "", cuina = "";
+				std::string wheelchair = "no";
+
+				// atributs
+				for (const auto& atribut : element.atributs)
+				{
+					// assignació coordenades
+					if (atribut.first == "lat")
+						lat = std::stod(atribut.second);
+					if (atribut.first == "lon")
+						lon = std::stod(atribut.second);
+				}
+
+				// fills  (aqui esta el shop, opening_hours...)
+				for (const auto& fill : element.fills)
+				{
+					// extreure una parella <key, value>, e. g. <shop, bakery>
+					std::pair<std::string, std::string> parella;
+					std::vector <PAIR_ATTR_VALUE> fillet = fill.second;
+					parella = Util::kvDeTag(fillet);
+
+					// deteccio de shop o restaurant
+					if (parella.first == "shop") {
+						botiga = true;
+						shop = parella.second;
+					}
+					else if (parella.first == "restaurant") {
+						restaurant = true;
+						rest = parella.second;
+					}
+
+					// altres atributs
+					if (parella.first == "wheelchair")
+						wheelchair = parella.second;
+					if (parella.first == "opening_hours")
+						hores = parella.second;
+					if (parella.first == "cuisine")
+						cuina = parella.second;
+				}
+
+				Coordinate coords = { lat, lon };
+
+				// crear PuntDeInteresBase / Botiga / Restaurant
+				if (botiga)
+				{
+					puntsDeInteres.push_back(new PuntDeInteresBotigaSolucio(coords, name, shop, hores, wheelchair));
+					Util::escriuEnMonitor("Botiga");
+				}
+				else if (restaurant)
+				{
+					puntsDeInteres.push_back(new PuntDeInteresRestaurantSolucio(coords, name, rest, wheelchair));
+					Util::escriuEnMonitor("Restaurant");
+				}
+				else
+				{
+					puntsDeInteres.push_back(new PuntDeInteresBase(coords, name));
+					Util::escriuEnMonitor("Node");
+				}
+			}
+			else if (element.id_element == "way")
+			{
+				Util::escriuEnMonitor("camins");
+			}
+		}
+	}
 };
+
 
 
